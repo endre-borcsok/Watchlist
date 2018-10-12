ac0ab2c8702501783d0167802c8e97789002876d
Unknown <unknown@example.com>
2018-10-12 16:21:27 +0100

    Simplified CreateWatchlistActivity.

--- app/src/main/java/com/ebsoft/watchlist/utils/BindingUtils.java
+++ app/src/main/java/com/ebsoft/watchlist/utils/BindingUtils.java
@@ -2,6 +2,10 @@ package com.ebsoft.watchlist.utils;
 
 import android.databinding.BindingAdapter;
 import android.support.v7.widget.RecyclerView;
+import android.view.KeyEvent;
+import android.view.inputmethod.EditorInfo;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.ebsoft.watchlist.ui.adapter.BindableAdapter;
 
@@ -16,6 +20,21 @@ public class BindingUtils {
         if (recyclerView.getAdapter() instanceof BindableAdapter) {
             ((BindableAdapter) recyclerView.getAdapter()).clearItems();
             ((BindableAdapter) recyclerView.getAdapter()).addItems(data);
+        }
+    }
+
+    @BindingAdapter({"imeActionClick"})
+    public static void bindImeActionClick(EditText editText, SoftKeyboardActionListener listener) {
+        if (listener == null) {
+            editText.setOnEditorActionListener(null);
+        } else {
+            editText.setOnEditorActionListener((textView, actionId, keyEvent) -> {
+                if (actionId == EditorInfo.IME_ACTION_DONE) {
+                    listener.onDoneClicked();
+                    return true;
+                }
+                return false;
+            });
         }
     }
 }
